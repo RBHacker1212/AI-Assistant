363c656fab8201d1cdb58917dd4449237d1f46b8
RBHacker1212 <cleancode1234@gmail.com>
2025-01-05 12:26:51 +0430

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from core.speech_recognition import listen_command
 from core.text_to_speech import speak
+from core.ai_engine import ask_openai
 
 def main():
+  speak("System is online. Hello! I am Your AI-Assistant. How can I help you?")
   while True:
     command = listen_command()
     if command:
-      if "hello"in command.lower():
-        speak("Hello! How can I help you today?")
-      elif "exit" in command.lower():
+      command_lower = command.lower()
+      if "exit" in command_lower:
         speak("Goodbye!")
         break
+      else:
+        response = ask_openai(command)
+        speak(response)
       else:
         speak("I didn`t understand that. Could you repeat?")
 
