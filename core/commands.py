68c513429157e7f3944f721cd44424ddbe450e03
RBHacker1212 <cleancode1234@gmail.com>
2025-01-05 12:36:29 +0430

    Update commands.py

--- core/commands.py
+++ core/commands.py
@@ -1,1 +1,16 @@
+import os webbrowser
 
+def open_application(app_name):
+  try:
+    if "browser" in app_name:
+      webbrowser.open("https://www.google.com")
+      return "Opening your browser."
+    elif "notepad" in app_name:
+      os.system("notepad")
+      return "Opening Notepad."
+    elif "calculator" in app_name:
+      return "Opening Calculator", os.system("calc")
+    else:
+      return "I cannot open that application yet."
+  except Exception as e:
+    return f"Error while opening  application: {e}"
