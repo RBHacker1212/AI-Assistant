9db699eab39c226bcf9facd2908ebf985a832909
RBHacker1212 <cleancode1234@gmail.com>
2025-01-05 12:13:40 +0430

    Update speech_recognition.py

--- core/speech_recognition.py
+++ core/speech_recognition.py
@@ -1,1 +1,17 @@
+import speech_recognition as sr
 
+def listen_command():
+  recognizer = sr.Recognizer()
+  with sr.Microphone() as source:
+    print("Listening for your command...")
+    try:
+      audio = recognizer.listen(source)
+      command = recognizer.recognize_google(audio)
+      print(f"You: {command}")
+      reeturn command
+    except sr.UnknownValueError:
+      print("Sorry, I could not understand your voice.")
+      return ""
+    except sr.RequestError:
+      print("There seems to be an issue with the speech recognition service.")
+      return ""
