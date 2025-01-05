4ab97501e12874691131be2a19c49a282922c6d8
RBHacker1212 <cleancode1234@gmail.com>
2025-01-05 12:08:19 +0430

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+from core.speech_recognition import listen_command
+from core.text_to_speech import speak
 
+def main():
+  while True:
+    command = listen_command()
+    if command:
+      if "hello"in command.lower():
+        speak("Hello! How can I help you today?")
+      elif "exit" in command.lower():
+        speak("Goodbye!")
+        break
+      else:
+        speak("I didn`t understand that. Could you repeat?")
+
+def __name__ == "__main__":
+  main()
